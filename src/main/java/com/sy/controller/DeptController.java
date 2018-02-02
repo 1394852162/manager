6202504e7c5695409c614c1a7c763eb8465460b0
李俊鹏 <1394852162@qq.com>
2018-02-02 17:10:28 +0800

    20180202 commit  to Branch......

--- src/main/java/com/sy/controller/DeptController.java
+++ src/main/java/com/sy/controller/DeptController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by haswell on 2018/1/25.
@@ -23,26 +25,24 @@ public class DeptController {
     private IDeptService DeptService;
     Dept dept = new Dept();
 
-    @RequestMapping("/list")
-    public HashMap<String,Object> GetDeptList(@RequestParam("name") String name){
-        int a = Integer.parseInt(name);
-       // dept.setDeptId(a);
-        List<Dept> list = this.DeptService.GetDeptList(a);
-        System.out.println(list);
-        HashMap<String,Object> map = new HashMap<String,Object>();
-        map.put("data","deptshuju");
-        return map;
+
+    @RequestMapping("/getDeptList.do")
+    @ResponseBody
+    public Map<String,Object>  GetDeptList(){
+        Map<String,Object> resultmap = new HashMap<String,Object>();
+        System.out.println("开始进入");
+        List<Dept> list = this.DeptService.GetQueryDeptList();
+        if(list != null & list.size()>0) {
+            resultmap.put("data",list);
+            resultmap.put("code",1);
+        }
+        else{
+            resultmap.put("data","查询失败");
+            resultmap.put("code",0);
+        }
+        return resultmap;
     }
 
-    @RequestMapping("/showUser")
-    public String toIndex(HttpServletRequest request, Model model){
-        System.out.println("开始进入");
-        int deptid = 1;
-        Dept dept = this.DeptService.getUserById(deptid);
-        System.out.println(dept.toString());
-        System.out.println(dept);
-        model.addAttribute("user", dept);
-        return "menu";
-    }
+
 
 }
