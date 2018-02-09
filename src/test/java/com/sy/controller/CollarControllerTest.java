83256130a657f3e6997e41475508d7b87b645c18
TaoPengFei <302665611@qq.com>
2018-02-08 23:19:10 -0800

    解决测试出现的问题：Can't find bundle for base name javax.servlet.LocalStrings, locale zh_CN; 原因在于没有引入javax.servlet-api包 ......

---
+++ src/test/java/com/sy/controller/CollarControllerTest.java
@@ -0,0 +1,56 @@
+package com.sy.controller;
+
+import javax.annotation.Resource;
+
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring-mvc.xml",
+        "classpath:spring-mybatis.xml"})
+public class CollarControllerTest {
+
+    @Resource
+    private CollarController collarController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(collarController).build();
+    }
+
+    @Test
+    public void testAddCollar() throws  Exception  {
+//		delete 需要传的参数和insert不一样，是 BrandSeq:1 pBrandId:[2,3] 这种json格式
+        JSONObject jo = new JSONObject();
+        jo.put("CollNo", "20180209");
+        jo.put("BatId", 13);
+        jo.put("BatEndTime", "2018-03-02");
+        jo.put("CollTime", "2018-02-09");
+        jo.put("EmpId", 2);
+        jo.put("CollNum", 8);
+        jo.put("CollNote", "春节第一次领取职工劵");
+//   这个就是前段需要传的json的array
+        /*
+
+        {"CollNo":"20180209","BatId":13,"BatEndTime":"2018-03-02","CollTime":"2018-02-09","EmpId":2,"CollNum":8,"CollNote":"春节第一次领取职工劵"}
+
+         */
+        String requestjson = jo.toString();
+        System.out.println(requestjson);
+//json格式的传输不能使用get方法中的paran这样的方法来处理了，必须是下面的方式
+        String responseString = mockMvc.perform(MockMvcRequestBuilders.post("/collar/insertCollarMap.do").contentType(MediaType.APPLICATION_JSON).content(requestjson))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(responseString);
+    }
+}
