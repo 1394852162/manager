4b6393885c5af455e477ea0914991486d2e87451
TaoPengFei <302665611@qq.com>
2018-03-19 04:00:47 -0400

    #23：添加一项“职工票使用情况查询”...

---
+++ src/main/webapp/org/cboard/controller/config/empticketCtrl.js
@@ -0,0 +1,48 @@
+/**
+ * Created by 陶鹏飞 on 2018/2/6.
+ */
+cBoard.controller("empticketCtrl",function ($rootScope, $scope, $http, dataService, $uibModal, ModalUtils, $filter) {
+
+    var translate = $filter('translate');
+    $scope.optFlag = 'none'; //editAuth
+    $scope.editAuth = {};
+
+    /**
+     * 修改项保存
+     */
+    $scope.saveEdit = function () {
+        /*$http({
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+            url: './employee/updateDeptEmpTree.do',
+            data: JSON.stringify({
+                EmpId: parseInt($scope.curEmpId),
+                DeptId: parseInt($scope.curDeptId),
+                EmpStatus3: $scope.editAuth.editEmpStatus3,
+                EmpStatus4: $scope.editAuth.editEmpStatus4,
+                EmpStatus5: $scope.editAuth.editEmpStatus5,
+                EmpStatus6: $scope.editAuth.editEmp1Status6,
+                EmpStatus7: $scope.editAuth.editDeptStatus7,
+                EmpStatus8: $scope.editAuth.editSearchTicketStatus8
+            })
+        }).success(function (response) {
+            if (response.code === 0) {
+                ModalUtils.alert(translate("设置失败!"), "modal-danger", "md");
+            } else if (response.code === 1) {
+                ModalUtils.alert(translate("设置成功!"), "modal-success", "md");
+                $scope.treeStatusArea = response.code;
+            } else if (response.code === -1) {
+                ModalUtils.alert(translate("设置失败!"), "modal-danger", "md");
+            }
+        }).error(function (XMLHttpRequest, textStatus, errorThrown) {
+            ModalUtils.alert(translate(errorThrown + "!"), "modal-danger", "sm");
+        });*/
+    }
+
+    /**
+     * 取消
+     */
+    $scope.canel = function () {
+        $scope.optFlag = 'none';
+    }
+})
