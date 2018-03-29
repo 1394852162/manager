c0236266653e3c996fc09fce2931faafd40a35ed
TaoPengFei <302665611@qq.com>
2018-03-29 02:32:27 -0400

    #44："Pentaho Report完成嵌入"...

--- src/main/java/prd/reporter/prdcontroller/ReporterController.java
+++ src/main/java/prd/reporter/prdcontroller/ReporterController.java
@@ -5,16 +5,22 @@ import org.pentaho.reporting.engine.classic.core.MasterReport;
 import org.pentaho.reporting.engine.classic.core.ReportProcessingException;
 import org.pentaho.reporting.engine.classic.core.modules.output.pageable.pdf.PdfReportUtil;
 import org.pentaho.reporting.engine.classic.core.modules.output.table.base.StreamReportProcessor;
-import org.pentaho.reporting.engine.classic.core.modules.output.table.html.*;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.html.AllItemsHtmlPrinter;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.html.FileSystemURLRewriter;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.html.HtmlPrinter;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.html.StreamHtmlOutputProcessor;
 import org.pentaho.reporting.engine.classic.core.modules.output.table.xls.ExcelReportUtil;
 import org.pentaho.reporting.libraries.repository.ContentIOException;
 import org.pentaho.reporting.libraries.repository.ContentLocation;
 import org.pentaho.reporting.libraries.repository.DefaultNameGenerator;
 import org.pentaho.reporting.libraries.repository.file.FileRepository;
-import org.pentaho.reporting.libraries.resourceloader.*;
+import org.pentaho.reporting.libraries.resourceloader.ResourceCreationException;
+import org.pentaho.reporting.libraries.resourceloader.ResourceKeyCreationException;
+import org.pentaho.reporting.libraries.resourceloader.ResourceLoadingException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import prd.reporter.AbstractReportGenerator;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import prd.reporter.prdpojo.Reporter;
 
 import javax.servlet.ServletException;
@@ -23,19 +29,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-//@SuppressWarnings("serial")
 @Controller
-//@RequestMapping("/reporter")
 public class ReporterController implements BaseController {
 
-
-    private static final Map<String, String> contentTypeMap = new HashMap<String, String>();
+    private static final Map<String, String> contentTypeMap = new HashMap<>();
     static {
         contentTypeMap.put("pdf", "application/pdf");
         contentTypeMap.put("excel2003", "application/vnd.ms-excel");
@@ -43,13 +45,6 @@ public class ReporterController implements BaseController {
         contentTypeMap.put("html", "text/html");
 
     }
-
-    /*@Override
-    void doReporter() {
-        *//*final MasterReport report = new Reporter().getReportDefinition();
-        report.getParameterValues();*//*
-    }*/
-
 
     @ResponseBody
     @RequestMapping("/getReporter")
@@ -60,7 +55,6 @@ public class ReporterController implements BaseController {
         final MasterReport report = new Reporter().getReportDefinition();
 
         Map<String,String> outputTypeMap = new HashMap<>();
-        outputTypeMap.put("1","1");
         String outputType = null;
 
         if ( null != paramMap ){
@@ -73,13 +67,27 @@ public class ReporterController implements BaseController {
                 if ("outputType" == key){
                     outputType = (String) value;
                 }
-//                System.out.println(key + ":" + value);
             }
         }
         request.setCharacterEncoding("utf-8");
         response.setContentType(contentTypeMap.get(outputType.toLowerCase()));
         if ( outputType.equalsIgnoreCase("pdf") ) {
-            OutputStream out = response.getOutputStream();
+            String fileName = Reporter.class.getSimpleName() + "." + outputType;
+            File folderOut = new File(request.getSession().getServletContext().getRealPath("/PDF"));
+            FileUtils.deleteDirectory(folderOut);
+            folderOut.mkdirs();
+
+            PdfReportUtil.createPDF(report,folderOut.getAbsolutePath() + "/" + fileName);
+            String route =  folderOut.getName() + "/" + fileName;
+            String path = request.getContextPath();
+            String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/"+route;
+            response.setContentType(contentTypeMap.get(outputType.toLowerCase()));
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+            response.getOutputStream();
+            outputTypeMap.put("PRD_URL",basePath);
+            outputTypeMap.put("OUT_PUT_TYPE",outputType);
+
+            /*OutputStream out = response.getOutputStream();
             System.out.println("out == " + out);
             try {
                 PdfReportUtil.createPDF(report, out);
@@ -87,10 +95,10 @@ public class ReporterController implements BaseController {
                 e.printStackTrace();
             } finally {
                 out.close();
-            }
+            }*/
         } else if ( outputType.equalsIgnoreCase("html") ) {
             String fileName = Reporter.class.getSimpleName() + "." + outputType;
-            File folderOut = new File(request.getSession().getServletContext().getRealPath("/out"));
+            File folderOut = new File(request.getSession().getServletContext().getRealPath("/HTML"));
             FileUtils.deleteDirectory(folderOut);
             folderOut.mkdirs();
             final FileRepository targetRepository = new FileRepository(folderOut);
@@ -105,59 +113,82 @@ public class ReporterController implements BaseController {
             reportProcessor.processReport();
             reportProcessor.close();
             String route =  folderOut.getName() + "/" + fileName;
+            String path = request.getContextPath();
+            String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/"+route;
 //            response.sendRedirect(route);
 //            request.getRequestDispatcher(route).forward(request, response);
 
-            System.out.println(route);
-            System.out.println(request.getSession().getServletContext().getRealPath("/"));
-//            System.out.println("=======================================================");
-            String path = request.getContextPath();
-            String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/"+route;
+            /*System.out.println(route);
+            System.out.println(request.getSession().getServletContext().getRealPath("/"));*/
+
 //            System.out.println(path);
-            System.out.println(basePath);
-//            System.out.println("=======================================================");
-            outputTypeMap.put("PRD_URL","1");
+//            System.out.println(basePath);
+            outputTypeMap.put("PRD_URL",basePath);
+            outputTypeMap.put("OUT_PUT_TYPE",outputType);
 
-
-
-//            return
-
-            OutputStream out = response.getOutputStream();
+            /*OutputStream out = response.getOutputStream();
             try {
                 HtmlReportUtil.createStreamHTML(report, out);
             } catch (ReportProcessingException e) {
                 e.printStackTrace();
             } finally {
                 out.close();
-            }
+            }*/
         } else if ( outputType.equalsIgnoreCase("excel2003")
                     || outputType.equalsIgnoreCase("excel2007") ) {
 //            response.setContentType("application/x-msdownload");
             if(outputType.equalsIgnoreCase("excel2003")) {
                 response.setHeader("Content-disposition", "attachment;filename="+ Reporter.class.getSimpleName() +".xls");
-                OutputStream out = response.getOutputStream();
+
+                String fileName = Reporter.class.getSimpleName() + ".xls";
+                File folderOut = new File(request.getSession().getServletContext().getRealPath("/EXCEL"));
+                FileUtils.deleteDirectory(folderOut);
+                folderOut.mkdirs();
+
+                ExcelReportUtil.createXLS(report,folderOut.getAbsolutePath() + "/" + fileName);
+
+                String route =  folderOut.getName() + "/" + fileName;
+                String path = request.getContextPath();
+                String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/"+route;
+                response.getOutputStream();
+                outputTypeMap.put("PRD_URL",basePath);
+                outputTypeMap.put("OUT_PUT_TYPE",outputType);
+
+                /*OutputStream out = response.getOutputStream();
                 try {
                     ExcelReportUtil.createXLS(report, out);
                 } catch (ReportProcessingException e) {
                     e.printStackTrace();
                 } finally {
                     out.close();
-                }
+                }*/
             } else if(outputType.equalsIgnoreCase("excel2007")){
                 response.setHeader("Content-disposition", "attachment;filename="+ Reporter.class.getSimpleName() +".xlsx");
-                OutputStream out = response.getOutputStream();
+
+                String fileName = Reporter.class.getSimpleName() + ".xlsx";
+                File folderOut = new File(request.getSession().getServletContext().getRealPath("/EXCEL"));
+                FileUtils.deleteDirectory(folderOut);
+                folderOut.mkdirs();
+
+                ExcelReportUtil.createXLSX(report,folderOut.getAbsolutePath() + "/" + fileName);
+
+                String route =  folderOut.getName() + "/" + fileName;
+                String path = request.getContextPath();
+                String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/"+route;
+                response.getOutputStream();
+                outputTypeMap.put("PRD_URL",basePath);
+                outputTypeMap.put("OUT_PUT_TYPE",outputType);
+
+                /*OutputStream out = response.getOutputStream();
                 try {
                     ExcelReportUtil.createXLSX(report, out);
                 } catch (ReportProcessingException e) {
                     e.printStackTrace();
                 }  finally {
                     out.close();
-                }
+                }*/
             }
         }
-
-
-
 //        System.out.println("outputType==" + outputType);
 //        System.out.println(response.getContentType());
 //        System.out.println(outputFilename.getAbsolutePath());
--- src/main/webapp/org/cboard/controller/config/empticketCtrl.js
+++ src/main/webapp/org/cboard/controller/config/empticketCtrl.js
@@ -4,16 +4,21 @@
 cBoard.controller("empticketCtrl",function ($rootScope, $scope, $http, dataService, $uibModal, ModalUtils, $filter) {
 
     var translate = $filter('translate');
-    $scope.optFlag = 'none'; //editAuth
-    $scope.editAuth = {};
 
+    ///表格的头部
+    $scope.oTypeInfos = [
+        {'id': 'html', 'name': 'HTML'},
+        {'id': 'pdf', 'name': 'PDF'},
+        {'id': 'excel2003', 'name': 'Excel 2003'},
+        {'id': 'excel2007', 'name': 'Excel 2007'}
+    ];
 
     $scope.testTicket = function () {
         $http({
             method: 'POST',
+            // headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
             headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
             // headers: {'Content-Type': 'application/vnd.ms-excel', 'Accept': 'application/vnd.ms-excel'},
-            // url: './reporter/getReporter',
             url: './getReporter',
             dataType:"json",
             data: JSON.stringify({
@@ -24,16 +29,7 @@ cBoard.controller("empticketCtrl",function ($rootScope, $scope, $http, dataServi
             })
         }).success(function (response) {
             console.log(response);
-            // $("#report").attr("src", response);
-            // $("#report").attr("src", "http://192.168.126.146:8080/manager/out/Reporter.html");
-            /*if (response.code === 0) {
-                ModalUtils.alert(translate("设置失败!"), "modal-danger", "md");
-            } else if (response.code === 1) {
-                ModalUtils.alert(translate("设置成功!"), "modal-success", "md");
-                $scope.treeStatusArea = response.code;
-            } else if (response.code === -1) {
-                ModalUtils.alert(translate("设置失败!"), "modal-danger", "md");
-            }*/
+            $("#report").attr("src", response.PRD_URL);
         }).error(function (XMLHttpRequest, textStatus, errorThrown) {
             ModalUtils.alert(translate(errorThrown + "!"), "modal-danger", "sm");
         });
